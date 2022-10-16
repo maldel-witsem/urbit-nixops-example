c8b4e0677df5e446d1ac7fa815c475fe69058ad9
maldel-witsem <radicle@maldel-witsem.xyz>
2022-10-16 14:19:40 -0600

    init

---
+++ flake.nix
@@ -0,0 +1,10 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/master";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      {});
+}
